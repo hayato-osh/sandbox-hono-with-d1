0308cda100aee22ea834c5b3f438854ccab3e658
Fastman <gabayt0_0@icloud.com>
2024-10-26 21:01:36 +0900

    feat: implement post endpoint

--- src/index.ts
+++ src/index.ts
@@ -22,4 +22,30 @@ app.get("/api/posts/:slug/comments", async (c) => {
   return c.json(results);
 });
 
+app.post("/api/posts/:slug/comments", async (c) => {
+  const { slug } = c.req.param();
+  const { author, body } = await c.req.json();
+
+  if (!author) return c.text("Missing author value for new comment");
+  if (!body) return c.text("Missing body value for new comment");
+
+  const db = await c.env.DB
+
+  const { success } = await db.prepare(
+    `
+    insert into comments (author, body, post_slug) values (?, ?, ?)
+  `,
+  )
+    .bind(author, body, slug)
+    .run();
+
+  if (success) {
+    c.status(201);
+    return c.text("Created");
+  } else {
+    c.status(500);
+    return c.text("Something went wrong");
+  }
+});
+
 export default app
