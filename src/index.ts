1dae90418a6fc1db72f6bff3b650ba91a7a33a0e
Fastman <gabayt0_0@icloud.com>
2024-10-26 18:20:48 +0900

    feat: create endpoint for comment

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/api/posts/:slug/comments", async (c) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with `c.req.param("slug")`
+});
+
+app.post("/api/posts/:slug/comments", async (c) => {
+  // Do something and return an HTTP response
+  // Optionally, do something with `c.req.param("slug")`
+});
 
 export default app
